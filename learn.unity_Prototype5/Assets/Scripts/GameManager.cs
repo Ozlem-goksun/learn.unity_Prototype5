c03998be4686300c7180a85bc0cad280ab22f81a
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-12-07 12:09:43 +0300

    added restart function

--- learn.unity_Prototype5/Assets/Scripts/GameManager.cs
+++ learn.unity_Prototype5/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,15 +12,20 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
 
+    public Button restartButton;
+
     private int score;
     private float targetsRate = 1.0f;
+
+    public bool isGameActive;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
 
+        StartCoroutine(SpawnTarget());
         UpdateScore(0);
     }
 
@@ -30,7 +37,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while(true)
+        while(isGameActive)
         {
             yield return new WaitForSeconds(targetsRate);
             int index = Random.Range(0, targets.Count);
@@ -47,7 +54,14 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
