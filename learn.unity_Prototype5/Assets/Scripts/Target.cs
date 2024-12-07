2eb3dfd734bbf5ead25073eaefc1a377928d9b86
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-12-07 10:52:13 +0300

    added game over scene

--- learn.unity_Prototype5/Assets/Scripts/Target.cs
+++ learn.unity_Prototype5/Assets/Scripts/Target.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
 
     private float minSpeed = 14.0f;
     private float maxSpeed = 17.5f;
     private float maxTorque = 10.0f;
     private float xRange = 4.0f;
-    private float ySpawnPos = -5.0f;
+    private float ySpawnPos = -2.0f;
+
+    public ParticleSystem explosionParticle;
+
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque());
@@ -26,6 +33,24 @@ public class Target : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 
     Vector3 RandomForce()
